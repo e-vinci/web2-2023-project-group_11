37a5ef85c3394b2e10b01c087ccfa33777a7c949
nathanael <nathanael.afangbedjee@student.vinci.be>
2023-11-13 23:16:27 +0100

    bootstrap changes

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,8 +1,8 @@
 const HomePage = () => {
   const main = document.querySelector('main');
   main.innerHTML = `
-  <div class="container px-4 text-center">
-  <div class="row gx-5">
+  <div class="container px-4 text-center" >
+  <div class="row gx-5 gap-2 row-gap-2">
     <div class="col">
      <div class="p-3">Custom column padding</div>
     </div>
@@ -12,7 +12,7 @@ const HomePage = () => {
   </div>
 </div>
 <div class="container px-4 text-center">
-  <div class="row gx-5">
+  <div class="row gx-5 gap-2 row-gap-2">
     <div class="col">
      <div class="p-3">Custom column padding</div>
     </div>
