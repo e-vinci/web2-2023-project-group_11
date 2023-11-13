e763443abf4c4c4c6afc67b877c987227a779baa
Nathanael Afangbedjee <nathanael.afangbedjee@student.vinci.be>
2023-11-13 12:47:01 +0100

    add

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,27 @@
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = '';
+  main.innerHTML = `
+  <div class="container px-4 text-center">
+  <div class="row gx-5">
+    <div class="col">
+     <div class="p-3">Custom column padding</div>
+    </div>
+    <div class="col">
+      <div class="p-3">Custom column padding</div>
+    </div>
+  </div>
+</div>
+<div class="container px-4 text-center">
+  <div class="row gx-5">
+    <div class="col">
+     <div class="p-3">Custom column padding</div>
+    </div>
+    <div class="col">
+      <div class="p-3">Custom column padding</div>
+    </div>
+  </div>
+</div>
+  `;
 };
 
 export default HomePage;
