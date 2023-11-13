8cb9e77f3f4f46038be4a34e984c616b7050a1a0
Ali El Hannouti <ali.elhannouti@student.vinci.be>
2023-11-13 12:47:39 +0100

    login and register beginning

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -10,7 +10,19 @@ const LoginPage = () => {
 
 function renderLoginPage() {
     const main = document.querySelector('main');
-    main.innerHTML = 'Login Page hh';
+    main.innerHTML = `<h1 class="text-center">Connectez vous pour enregistrer votre score !</h1>
+    <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="loginPassword">Password</label>
+      <input type="password" class="form-control" id="loginPassword" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary">Se connecter</button>
+  </form>`;
   };
 
   export default LoginPage;
--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -10,7 +10,23 @@ const RegisterPage = () => {
 
 function renderRegisterPage() {
     const main = document.querySelector('main');
-    main.innerHTML = 'Register Page';
-  };
-
+    main.innerHTML = `<h1 class="text-center">Inscrivez-vous pour tenter de vous trouver dans le tableau des meilleurs score !</h1>
+    <div class="center-form"><form>
+      <div class="form-group">
+        <label for="emailRegister">Email address</label>
+        <input type="email" class="form-control" id="emailRegister" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="usernameRegister">Username</label>
+        <input type="text" class="form-control" id="emailRegister" aria-describedby="usernameHelp" placeholder="Enter username">
+      </div>
+      <div class="form-group">
+        <label for="passwordRegister">Password</label>
+        <input type="password" class="form-control" id="registerPassword" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary">S'inscrire</button>
+    </form></div>
+  `;
+};
   export default RegisterPage;
