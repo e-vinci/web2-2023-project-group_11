281c6da80243e52d74e3d3b900eb85306e7f8478
rafael-millor-vinci <rafael.millor@student.vinci.be>
2023-11-13 12:46:22 +0100

    add : animation

--- frontend/src/Components/Pages/QuizzPage.js
+++ frontend/src/Components/Pages/QuizzPage.js
@@ -1,4 +1,7 @@
+import anime from 'animejs';
 import { clearPage, renderPageTitle } from '../../utils/render';
+
+
 
 const QuizzPage = () => {
   clearPage();
@@ -8,8 +11,24 @@ const QuizzPage = () => {
 
 function renderQuizzPage() {
   const main = document.querySelector('main');
-  main.innerHTML = '<canvas />';
-  
+
+  main.innerHTML = `<div class="animTest">
+  <div class="p-5 anime-test-1">
+  <input type="button" value="Bouton cliquer" />
+  </div>
+  <div class="p-5 anime-test-2">
+  <input type="button" value="Bouton cliquer" />
+  </div>
+ </div>
+ `;
+
+ anime({
+  targets: [".anime-test-1",".anime-test-2"],
+  translateX: 70,
+  translateY: 170,
+  delay: anime.stagger(300) // increase delay by 100ms for each elements.
+});
+
 }
 
 export default QuizzPage;
