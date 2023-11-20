35b49adfd125fa625df86e4c06c8bbd6b1cec8e7
Lucas Gregoire <lucas.gregoire@student.vinci.be>
2023-11-20 12:43:58 +0100

    quizzpage2

--- frontend/src/Components/Pages/QuestionPage.js
+++ frontend/src/Components/Pages/QuestionPage.js
@@ -5,48 +5,64 @@ import { start } from '@popperjs/core';
 
 let questionId;
 
+const questions = [{ id: 1, 
+  question: 'Quelle couleur est le ciel ?',
+ answers: [{ text: "bleu", isCorrect: true }, 
+ { 
+  text: "vert",
+  isCorrect: false }, { text: "rouge", isCorrect: false }, 
+ { text: "orange", isCorrect: false }] },];
+
 const QuestionPage = () => {
   clearPage();
   renderPageTitle('Question');
   renderQuestionPage();
 };
 
-if(!questionId){
+if (!questionId) {
   Navigate('/home');
 }
 
 function renderQuestionPage() {
-  
- const main = document.querySelector('main');
 
- const banniere = document.createElement('div');
- banniere.className = "banner"
- banniere.innerHTML
+  const main = document.querySelector('main');
 
- const textElement = document.createElement('span');
- textElement.className = "text-element";
+  const banniere = document.createElement('div');
+  banniere.className = "banner"
+  banniere.innerHTML
 
- const startGame = document.createElement('button');
- startGame.className = "start-button";
- startGame.innerText = "Commencer";
+  const textElement = document.createElement('span');
+  textElement.className = "text-element";
 
- const questionDiv = document.createElement("div");
- questionDiv.className = "question-div";
+  const startGame = document.createElement('button');
+  startGame.className = "start-button";
+  startGame.innerText = "Commencer";
 
- const titleDiv = document.createElement('div');
- titleDiv.className = "question-container";
+  const questionDiv = document.createElement("div");
+  questionDiv.className = "question-div";
 
- const titleQuestion = document.createElement('h1');
- titleQuestion.className = "question-title";
+  const titleDiv = document.createElement('div');
+  titleDiv.className = "question-container";
 
- titleQuestion.appendChild(textElement);
- titleDiv.appendChild(titleQuestion);
- questionDiv.appendChild(titleDiv);
- banniere.appendChild(questionDiv);
- main.appendChild(banniere);
+  const titleQuestion = document.createElement('h1');
+  titleQuestion.className = "question-title";
+
+  titleQuestion.appendChild(textElement);
+  titleDiv.appendChild(titleQuestion);
+  questionDiv.appendChild(titleDiv);
+  banniere.appendChild(questionDiv);
+  main.appendChild(banniere);
 
   main.innerHTML = '<canvas />';
-  
+
+  const startQuizz = () => {
+    startGame.removeEventListener('click', startQuizz);
+    for (const question in questions) {
+
+    }
+  }
+
+  startGame.addEventListener('click', startQuizz);
 }
 
 export default QuestionPage;
