2abf77cf56a1f56e0272b66483b1461058f93f81
Nathanael Afangbedjee <nathanael.afangbedjee@student.vinci.be>
2023-11-20 12:04:56 +0100

    update

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,23 +1,23 @@
 const HomePage = () => {
   const main = document.querySelector('main');
   main.innerHTML = `
-  <div class="container px-4 text-center" >
+  <div class="container px-4 text-center mb-20">
   <div class="row gx-5 gap-2 row-gap-2">
     <div class="col">
-     <div class="p-3">Custom column padding</div>
+     <div class="p-3"> Sport </div>
     </div>
     <div class="col">
-      <div class="p-3">Custom column padding</div>
+      <div class="p-3"> Animaux </div>
     </div>
   </div>
 </div>
 <div class="container px-4 text-center">
   <div class="row gx-5 gap-2 row-gap-2">
     <div class="col">
-     <div class="p-3">Custom column padding</div>
+     <div class="p-3"> Jeux </div>
     </div>
     <div class="col">
-      <div class="p-3">Custom column padding</div>
+      <div class="p-3"> Poulet </div>
     </div>
   </div>
 </div>
