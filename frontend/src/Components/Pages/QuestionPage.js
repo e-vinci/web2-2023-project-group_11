25aa31ec3df5cad1a5b2ac868e1522c3250b3b4a
Lucas Gregoire <lucas.gregoire@student.vinci.be>
2023-11-23 11:38:16 +0100

    boucle de jeu

--- frontend/src/Components/Pages/QuestionPage.js
+++ frontend/src/Components/Pages/QuestionPage.js
@@ -6,7 +6,7 @@ import { start } from '@popperjs/core';
 let questionId;
 
 const questions = [{ id: 1, 
-  question: 'Quelle couleur est le ciel ?',
+  title: 'Quelle couleur est le ciel ?',
  answers: [{ text: "bleu", isCorrect: true }, 
  { 
   text: "vert",
@@ -44,6 +44,18 @@ function renderQuestionPage() {
   const titleDiv = document.createElement('div');
   titleDiv.className = "question-container";
 
+  const answer1Div = document.createElement('div');
+  answer1Div.className = "answer1-container";
+
+  const answer2Div = document.createElement('div');
+  answer2Div.className = "answer2-container";
+
+  const answer3Div = document.createElement('div');
+  answer3Div.className = "answer3-container";
+
+  const answer4Div = document.createElement('div');
+  answer4Div.className = "answer4-container";
+
   const titleQuestion = document.createElement('h1');
   titleQuestion.className = "question-title";
 
@@ -52,13 +64,34 @@ function renderQuestionPage() {
   questionDiv.appendChild(titleDiv);
   banniere.appendChild(questionDiv);
   main.appendChild(banniere);
-
+  main.appendChild(answer1Div);
+  main.appendChild(answer2Div);
+  main.appendChild(answer3Div);
+  main.appendChild(answer4Div);
   main.innerHTML = '<canvas />';
 
   const startQuizz = () => {
     startGame.removeEventListener('click', startQuizz);
     for (const question in questions) {
-
+      let idRep = 0;
+       clearPage();
+       main.innerHTML = 
+       `
+       <div class="titleDiv">
+         <p> ${question.title} </p>
+       <div>
+       <container>
+        <div>
+          <p> ${question.answers}</p>
+        <div>
+        <div>
+          <p> </p>
+        <div>
+        <div>
+          <p> </p>
+        <div>
+       <container>
+       `
     }
   }
 
