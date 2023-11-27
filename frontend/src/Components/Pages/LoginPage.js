6f8ce68337117aa7eeb740c53e7654e40799b4ce
Ali El Hannouti <ali.elhannouti@student.vinci.be>
2023-11-27 12:48:37 +0100

    logo navbar et login formulaire bootstrap

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -12,16 +12,17 @@ function renderLoginPage() {
     const main = document.querySelector('main');
     main.innerHTML = `<h1 class="text-center">Connectez vous pour enregistrer votre score !</h1>
     <form>
-    <div class="form-group">
+    <div class="form-group w-50 p-3">
       <label for="exampleInputEmail1">Email address</label>
       <input type="email" class="form-control" id="input_email_login" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
-    <div class="form-group">
+    <div class="form-group w-50 p-3">
       <label for="loginPassword">Password</label>
       <input type="password" class="form-control" id="input_password_login" placeholder="Password">
+      <small id="pswHelp" class="form-text text-muted">We'll never share your password with anyone else.</small>
     </div>
-    <button type="submit" class="btn btn-primary">Se connecter</button>
+    <div class="form-group p-3"><button type="submit" class="btn btn-primary " >Se connecter</button>
+    </div>
   </form>`;
   };
 
--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -5,27 +5,26 @@ const RegisterPage = () => {
     clearPage();
     renderPageTitle('Register');
     renderRegisterPage();
-
   };
 
 function renderRegisterPage() {
     const main = document.querySelector('main');
     main.innerHTML = `<h1 class="text-center">Inscrivez-vous pour tenter de vous trouver dans le tableau des meilleurs score !</h1>
     <div class="center-form"><form>
-      <div class="form-group">
+      <div class="form-group w-50 p-3">
         <label for="emailRegister">Email address</label>
         <input type="email" class="form-control" id="emailRegister" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
-      <div class="form-group">
+      <div class="form-group w-50 p-3">
         <label for="usernameRegister">Username</label>
         <input type="text" class="form-control" id="emailRegister" aria-describedby="usernameHelp" placeholder="Enter username">
       </div>
-      <div class="form-group">
+      <div class="form-group w-50 p-3">
         <label for="passwordRegister">Password</label>
         <input type="password" class="form-control" id="registerPassword" placeholder="Password">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your password with anyone else.</small>
       </div>
-      <button type="submit" class="btn btn-primary">S'inscrire</button>
+      <div class="form-group p-3"><button type="submit" class="btn btn-primary">S'inscrire</button></div>
     </form></div>
   `;
 };
