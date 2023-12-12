48c589ad342c356fd4374a9725aeb751d9092a19
rafael-millor-vinci <rafael.millor@student.vinci.be>
2023-12-12 15:14:32 +0100

    reparer leaderbard

--- frontend/src/Components/Pages/LeaderBoardPage.js
+++ frontend/src/Components/Pages/LeaderBoardPage.js
@@ -1,12 +1,9 @@
 import { clearPage, renderPageTitle } from '../../utils/render';
 
-const dataNonTrie = await fetchUsers();
-const leaderboardData = dataNonTrie.sort(
-  ((a, b) =>
-     (b.score / b.nbPartie === 0 ? 1 : b.nbPartie)- (a.score / a.nbPartie === 0 ? 1 : a.nbPartie)),
-);
+
 async function fetchUsers() {
   try {
+    
     const response = await fetch(`${process.env.API_BASE_URL}/users`);
 
     if (!response.ok) {
@@ -21,39 +18,48 @@ async function fetchUsers() {
   return [];
 
 }
-const LeaderBoardPage = () => {
+async function  LeaderBoardPage  () {
+
+  const dataNonTrie = await fetchUsers();
+
+  const leaderboardData = dataNonTrie.sort(
+    ((a, b) =>
+       (b.score / b.nbPartie === 0 ? 1 : b.nbPartie)- (a.score / a.nbPartie === 0 ? 1 : a.nbPartie)),
+  );
+  function renderLeaderBoardPage() {
+    const main = document.querySelector('main');
+    let rank = 0;
+    main.innerHTML = `
+      <table class="leaderboard">
+          <thead>
+              <tr>
+                  <th>Rank</th>
+                  <th>Name</th>
+                  <th>Score</th>
+              </tr>
+          </thead>
+          <tbody>
+              ${leaderboardData
+                .map((player) => {
+                  rank += 1;
+                  return `
+                  <tr>
+                      <td>${rank}</td>
+                      <td>${player.username}</td>
+                      <td>${(player.score/(player.nbPartie === 0 ? 1 : player.nbPartie))}</td>
+                  </tr>
+              `;
+                })
+                .join('')}
+          </tbody>
+      </table>`;
+  }
+  
   clearPage();
+  console.log('clear page');
   renderPageTitle('Register');
   renderLeaderBoardPage();
 };
 
-function renderLeaderBoardPage() {
-  const main = document.querySelector('main');
-  let rank = 0;
-  main.innerHTML = `
-    <table class="leaderboard">
-        <thead>
-            <tr>
-                <th>Rank</th>
-                <th>Name</th>
-                <th>Score</th>
-            </tr>
-        </thead>
-        <tbody>
-            ${leaderboardData
-              .map((player) => {
-                rank += 1;
-                return `
-                <tr>
-                    <td>${rank}</td>
-                    <td>${player.username}</td>
-                    <td>${(player.score/(player.nbPartie === 0 ? 1 : player.nbPartie))}</td>
-                </tr>
-            `;
-              })
-              .join('')}
-        </tbody>
-    </table>`;
-}
 
 export default LeaderBoardPage;
