19bcbf1eaac9851be0ebc10cec896928ae243a4b
rafael-millor-vinci <rafael.millor@student.vinci.be>
2023-12-13 21:51:57 +0100

    fix

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'https://e-baron.github.io','https://rafael-millor-vinci.github.io'],
+  origin: ['http://localhost:8080', 'https://e-baron.github.io', 'https://rafael-millor-vinci.github.io'],
 };
 
 const usersRouter = require('./routes/users');
