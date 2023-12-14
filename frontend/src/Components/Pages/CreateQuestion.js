3340888e57b27e97cbe0f46060b9dee57f937a30
lucas-gregoire-vinci <lucas.gregoire@student.vinci.be>
2023-12-14 19:18:28 +0100

    prototype createQuestion v1

--- frontend/src/Components/Pages/CreateQuestion.js
+++ frontend/src/Components/Pages/CreateQuestion.js
@@ -1,14 +1,24 @@
+/* eslint-disable no-unused-vars */
 import clearPage from '../../utils/render';
 /* eslint-disable spaced-comment */
 const CreateQuestion = () => {
     clearPage();
    
-  
-    /*const form = document.getElementById('form');
-    if (form != null) {
-      form.addEventListener('submit', onFormSubmit)
-    }*/
-  
-  };
+    const form = document.createElement('form');
+    form.id = 'questionForm';
+
+  const createInput = (labelText, inputId, inputName, isCheckbox = false) => {
+    const label = document.createElement('label');
+    label.textContent = labelText;
+
+    const input = isCheckbox ? document.createElement('input') : document.createElement('textarea');
+    input.type = isCheckbox ? 'checkbox' : 'text';
+    input.id = inputId;
+    input.name = inputName;
+
+    form.appendChild(label);
+    form.appendChild(input);
+ };
+}
 
 export default CreateQuestion;
