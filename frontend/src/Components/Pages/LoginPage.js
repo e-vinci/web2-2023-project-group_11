912c93d2d0af4354b5c438fa16f6f1ad95740db0
ali-elhannouti-vinci <ali.elhannouti@student.vinci.be>
2023-12-15 01:39:59 +0100

    pop up erreurs formulaires login et register + arrangement disposition

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -1,7 +1,7 @@
 /* eslint-disable spaced-comment */
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line import/no-extraneous-dependencies
-//import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 import { setAuthenticatedUser } from '../../utils/auth';
 import { clearPage, renderPageTitle } from '../../utils/render';
 import Navbar from '../Navbar/Navbar';
@@ -15,21 +15,22 @@ const LoginPage = () => {
 
 function renderLoginPage() {
   const main = document.querySelector('main');
-  main.innerHTML = `<h1 class="text-center">Connectez vous pour enregistrer votre score !</h1>
+  main.innerHTML = `<div class="police-options">
+  <h1 class="text-center">Connectes-toi pour sauvegarder ton score!</h1>
     <form>
-    <div class="form-group w-50 p-3">
-      <label for="username">Username</label>
+    <div class="form-group w-50 p-3 d-grid gap-2 col-6 mx-auto">
+      <label for="username">Nom d'utilisateur</label>
       <input type="text" class="form-control" id="usernameLogin" aria-describedby="usernameHelp" placeholder="Enter username">
     </div>
-    <div class="form-group w-50 p-3">
-      <label for="loginPassword">Password</label>
+    <div class="form-group w-50 p-3 d-grid gap-2 col-6 mx-auto">
+      <label for="loginPassword">Mot de passe</label>
       <input type="password" class="form-control" id="passwordLogin" placeholder="Password">
-      <small id="pswHelp" class="form-text text-muted">We'll never share your password with anyone else.</small>
+      <small id="pswHelp">Nous ne partagerons jamais votre mot de passe avec qui que ce soit.</small>
     </div>
-    <div class="form-group p-3"><button type="submit" class="btn btn-primary " >Se connecter</button>
+    <div class="form-group p-3 d-grid gap-2 col-6 mx-auto"><button type="submit" class="btn btn-primary " >Se connecter</button>
     </div>
   </form>
-  </footer>`;
+  </div>`;
   const form = main.querySelector('form');
   form.addEventListener('submit', onLogin);
 }
@@ -41,15 +42,15 @@ async function onLogin(e) {
   const password = document.querySelector('#passwordLogin').value;
 
   if (!username || !password) {
-    /*popError('Il manque un champs obligatoire');*/
+    popErrorEmpty('Sois pas idiot, comment tu veux qu\'on te connecte au site si t\'es pas capable de remplir toutes les cases du formulaire ?');
     return;
   }
 
   const options = {
     method: 'POST',
     body: JSON.stringify({
-      username,
-      password,
+    username,
+    password,
     }),
     headers: {
       'Content-Type': 'application/json',
@@ -60,7 +61,7 @@ async function onLogin(e) {
     const response = await fetch(`${process.env.API_BASE_URL}/auths/login`, options);
 
     if (!response.ok) {
-      /*popError('Le pseudo ou le mot de passe est incorrect');*/
+      popErrorWrongInput('Le pseudo ou le mot de passe est incorrect. Bravo, tu as réussis à te tromper hors du quizz.');
       return;
     }
     const authenticatedUser = await response.json();
@@ -73,18 +74,42 @@ async function onLogin(e) {
 
     Navigate('/');
   } catch (err) {
-    /*popError('Une erreur est survenue');*/
+    popError('Une erreur est survenue');
     console.error('Connexion Error:', err);
   }
 }
 
-/*function popError(message) {
+function popError(message) {
   Swal.fire({
     icon: 'error',
     title: 'Oops...',
     text: message,
     showConfirmButton: true,
   });
-}*/
+}
+
+function popErrorEmpty(message) {
+  Swal.fire({
+    icon: 'question',
+    title: 'Oops...',
+    text: message,
+    showConfirmButton: true,
+    confirmButtonText: `
+    Je serai moins idiot lors de la prochaine tentative
+  `
+  });
+}
+
+function popErrorWrongInput(message) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: message,
+    showConfirmButton: true,
+    confirmButtonText: `
+    Désolé pour mon incompétence.
+  `
+  });
+}
 
 export default LoginPage;
--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -1,6 +1,6 @@
 /* eslint-disable spaced-comment */
 // eslint-disable-next-line import/no-extraneous-dependencies
-//import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 import { setAuthenticatedUser } from '../../utils/auth';
 import { clearPage, renderPageTitle } from '../../utils/render';
 import Navbar from '../Navbar/Navbar';
@@ -15,20 +15,20 @@ const RegisterPage = () => {
 function renderRegisterPage() {
   const main = document.querySelector('main');
 
-  main.innerHTML = `<h1 class="text-center">Inscrivez-vous pour tenter de vous trouver dans le tableau des meilleurs score !</h1>
-    <div class="center-form"><form>
-      <div class="form-group w-50 p-3">
-        <label for="usernameRegister">Username</label>
-        <input type="text" class="form-control" id="usernameRegister" aria-describedby="usernameHelp" placeholder="Enter username">
-      </div>
-      <div class="form-group w-50 p-3">
-        <label for="passwordRegister">Password</label>
-        <input type="password" class="form-control" id="registerPassword" placeholder="Password" minlength="8">
-        <small id="passwordHelp" class="form-text text-muted">We'll never share your password with anyone else.</small>
-      </div>
-      <div class="form-group p-3"><button type="submit" class="btn btn-primary" value="Register">S'inscrire</button></div>
-    </form></div>
-  `;
+  main.innerHTML = `<div class="police-options">
+  <h1 class="text-center">Inscrivez-vous pour tenter de vous placer en haut du tableau d'affichage !</h1>
+  <div class="center-form"><form>
+    <div class="form-group w-50 p-3 d-grid gap-2 col-6 mx-auto">
+      <label for="usernameRegister">Nom d'utilisateur</label>
+      <input type="text" class="form-control" id="usernameRegister" aria-describedby="usernameHelp" placeholder="Enter username">
+    </div>
+    <div class="form-group w-50 p-3 d-grid gap-2 col-6 mx-auto">
+      <label for="passwordRegister">Mot de passe</label>
+      <input type="password" class="form-control" id="passwordRegister" placeholder="Password" minlength="8">
+      <small id="passwordHelp">Nous ne partagerons jamais votre mot de passe avec qui que ce soit. De plus, votre mot de passe doit comporter au moins 8 caractères.</small>
+    </div>
+    <div class="form-group p-3 d-grid gap-2 col-6 mx-auto"><button type="submit" class="btn btn-primary" value="Register">S'inscrire</button></div>
+  </form></div>`;
   // eslint-disable-next-line no-unused-vars
   const form = main.querySelector('form');
   form.addEventListener('submit', onRegister);
@@ -38,10 +38,10 @@ async function onRegister(e) {
   e.preventDefault();
 
   const username = document.querySelector('#usernameRegister').value;
-  const password = document.querySelector('#registerPassword').value;
+  const password = document.querySelector('#passwordRegister').value;
 
   if (!username || !password) {
-    /*popError('Il manque un champs obligatoire');*/
+    popErrorEmpty('Sois pas idiot, comment tu veux qu\'on t\'inscrive au site si t\'es pas capable de remplir toutes les cases du formulaire ?');
     return;
   }
   try {
@@ -59,7 +59,7 @@ async function onRegister(e) {
     const response = await fetch(`${process.env.API_BASE_URL}/auths/register`, options);
 
     if (!response.ok) {
-      /*popError('Le pseudo existe deja');*/
+      popErrorUsernameTaken('Sois plus original pour ton pseudo, quelqu\'un l\'a déjà pris.');
       return;
     }
 
@@ -73,16 +73,41 @@ async function onRegister(e) {
 
     Navigate('/');
   } catch (err) {
-    //popError('Une erreur est survenue');
+    popError('Une erreur est survenue');
     console.error('Register Error:', err);
   }
 }
-/*function popError(message) {
+function popError(message) {
   Swal.fire({
     icon: 'error',
     title: 'Oops...',
     text: message,
     showConfirmButton: true,
   });
-} */
+}
+
+function popErrorEmpty(message) {
+  Swal.fire({
+    icon: 'question',
+    title: 'Abruti détecté',
+    text: message,
+    showConfirmButton: true,
+    confirmButtonText: `
+    Je serai moins idiot lors de la prochaine tentative
+  `
+  });
+}
+
+function popErrorUsernameTaken(message) {
+  Swal.fire({
+    icon: 'info',
+    title: 'Pseudo indisponible',
+    text: message,
+    showConfirmButton: true,
+    confirmButtonText: `
+    Je serai plus créatif.
+  `
+  });
+}
+
 export default RegisterPage;
